4886f9943124a517c8095aee9cf5a609c8928d39
Dmitrii <99279608+Dmitrii33rus@users.noreply.github.com>
2024-09-19 15:43:49 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+   
+      ecomStartWidget({                                                   
+          id: 43559,                                                         
+          containerId: 'ecom-widget-map',                                   
+          weight: 1000,                                                     
+          sumoc: 500,                                                                                 
+          start_location: ''                    
+                          });  
+          courierStartWidget({
+                id: 43560,
+               weight: 740,                              
+               sumoc: 500,
+          containerId: 'ecom-widget-courier'
+                            });
